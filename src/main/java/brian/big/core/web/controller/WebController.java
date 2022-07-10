a3b49b11806e58be91640b95630b9c24a35b7eb3
brian-big <briannbig@gmail.com>
2022-07-11 02:01:59 +0300

    fix thymeleaf deps, homepage

---
+++ src/main/java/brian/big/core/web/controller/WebController.java
@@ -0,0 +1,17 @@
+package brian.big.core.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("username", "Brian fox");
+        return "index";
+    }
+
+}
