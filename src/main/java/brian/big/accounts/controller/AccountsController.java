813235eb4870883398efa52debd03ce9ec793324
brian-big <briannbig@gmail.com>
2022-07-12 12:38:40 +0300

    login page

---
+++ src/main/java/brian/big/accounts/controller/AccountsController.java
@@ -0,0 +1,14 @@
+package brian.big.accounts.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class AccountsController {
+
+    @RequestMapping("/login")
+    public String loginPage(){
+        return "accounts/login";
+    }
+}
