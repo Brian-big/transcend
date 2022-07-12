761f087d87bc11db83091cd3b9e93b90b0be5fa1
brian-big <briannbig@gmail.com>
2022-07-12 11:38:46 +0300

    authenticate in memory authentication

--- src/main/java/brian/big/accounts/services/AppSecurityConfig.java
+++ src/main/java/brian/big/accounts/services/AppSecurityConfig.java
@@ -1,13 +1,28 @@
 package brian.big.accounts.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder encoder;
+
+    @Autowired
+    public AppSecurityConfig(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -16,5 +31,19 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails developerDetails = User.builder()
+                .username("developer")
+                .password(encoder.encode("developer"))
+                .roles("DEVELOPER")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                developerDetails
+        );
     }
 }
