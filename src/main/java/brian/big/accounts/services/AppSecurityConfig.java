d29db26793eeb66ae48bb866c7a733af123169d7
Brian Barasa <briannbig@gmail.com>
2023-12-25 06:56:51 +0300

    Update sprig security configuration as per v6

--- src/main/java/brian/big/accounts/services/AppSecurityConfig.java
+++ src/main/java/brian/big/accounts/services/AppSecurityConfig.java
@@ -3,22 +3,16 @@ package brian.big.accounts.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import static brian.big.accounts.services.AppUserPermissions.*;
-import static brian.big.accounts.services.AppUserRoles.*;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+public class AppSecurityConfig {
 
     private final PasswordEncoder encoder;
 
@@ -27,45 +21,59 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         this.encoder = encoder;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable() //TODO: fix this
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(STUDENT_WRITE.name())
-                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(STUDENT_WRITE.name())
-                .antMatchers(HttpMethod.PUT, "/api/**").hasAuthority(STUDENT_WRITE.name())
-                .antMatchers("/api/**").hasAnyRole(ADMIN.name(), DEVELOPER.name())
-                .and()
-                .httpBasic();
-    }
-
     @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails developerDetails = User.builder()
-                .username("developer")
-                .password(encoder.encode("developer"))
-                .roles(DEVELOPER.name())
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        (auth) -> auth
+                                .anyRequest().authenticated()
+                )
                 .build();
-
-        UserDetails userBrian = User.builder()
-                .username("brian")
-                .password(encoder.encode("passwordAdmin"))
-                .roles(ADMIN.name())
-                .build();
-
-        UserDetails userMerlin = User.builder()
-                .username("merlin")
-                .password(encoder.encode("merlin"))
-                .roles(STUDENT.name())
-                .build();
-
-
-        return new InMemoryUserDetailsManager(
-                developerDetails,
-                userBrian,
-                userMerlin
-        );
     }
+
+
+    //
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable() //TODO: fix this
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(STUDENT_WRITE.name())
+//                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(STUDENT_WRITE.name())
+//                .antMatchers(HttpMethod.PUT, "/api/**").hasAuthority(STUDENT_WRITE.name())
+//                .antMatchers("/api/**").hasAnyRole(ADMIN.name(), DEVELOPER.name())
+//                .and()
+//                .httpBasic();
+//    }
+
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails developerDetails = User.builder()
+//                .username("developer")
+//                .password(encoder.encode("developer"))
+//                .roles(DEVELOPER.name())
+//                .build();
+//
+//        UserDetails userBrian = User.builder()
+//                .username("brian")
+//                .password(encoder.encode("passwordAdmin"))
+//                .roles(ADMIN.name())
+//                .build();
+//
+//        UserDetails userMerlin = User.builder()
+//                .username("merlin")
+//                .password(encoder.encode("merlin"))
+//                .roles(STUDENT.name())
+//                .build();
+//
+//
+//        return new InMemoryUserDetailsManager(
+//                developerDetails,
+//                userBrian,
+//                userMerlin
+//        );
+//    }
 }
