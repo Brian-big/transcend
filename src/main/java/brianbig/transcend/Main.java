5997f924b2358b6847e97d282128e071265f646e
Brian Barasa <briannbig@gmail.com>
2023-12-25 09:07:50 +0300

    Add in missing Enable Config properties for JWTConfigProperties.class

--- src/main/java/brianbig/transcend/Main.java
+++ src/main/java/brianbig/transcend/Main.java
@@ -1,9 +1,12 @@
 package brianbig.transcend;
 
+import brianbig.transcend.config.JWTConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JWTConfigProperties.class)
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
