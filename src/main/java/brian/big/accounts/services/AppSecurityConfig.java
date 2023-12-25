9892b4660939dfde4305d45840f3d0cdbd47b556
Brian Barasa <briannbig@gmail.com>
2023-12-25 07:56:03 +0300

    Simplify password encoder configuration

--- src/main/java/brian/big/accounts/services/AppSecurityConfig.java
+++ src/main/java/brian/big/accounts/services/AppSecurityConfig.java
@@ -1,25 +1,18 @@
 package brian.big.accounts.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig {
-
-    private final PasswordEncoder encoder;
-
-    @Autowired
-    public AppSecurityConfig(PasswordEncoder encoder) {
-        this.encoder = encoder;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -32,48 +25,8 @@ public class AppSecurityConfig {
                 )
                 .build();
     }
-
-
-    //
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable() //TODO: fix this
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(STUDENT_WRITE.name())
-//                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(STUDENT_WRITE.name())
-//                .antMatchers(HttpMethod.PUT, "/api/**").hasAuthority(STUDENT_WRITE.name())
-//                .antMatchers("/api/**").hasAnyRole(ADMIN.name(), DEVELOPER.name())
-//                .and()
-//                .httpBasic();
-//    }
-
-//    @Bean
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        UserDetails developerDetails = User.builder()
-//                .username("developer")
-//                .password(encoder.encode("developer"))
-//                .roles(DEVELOPER.name())
-//                .build();
-//
-//        UserDetails userBrian = User.builder()
-//                .username("brian")
-//                .password(encoder.encode("passwordAdmin"))
-//                .roles(ADMIN.name())
-//                .build();
-//
-//        UserDetails userMerlin = User.builder()
-//                .username("merlin")
-//                .password(encoder.encode("merlin"))
-//                .roles(STUDENT.name())
-//                .build();
-//
-//
-//        return new InMemoryUserDetailsManager(
-//                developerDetails,
-//                userBrian,
-//                userMerlin
-//        );
-//    }
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder(10);
+    }
 }
